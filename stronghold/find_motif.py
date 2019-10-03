ad49fab074d45a8318c6c7eeb02d40a4f0cff2fa
Austin Vecchio <au.vecchio@gmail.com>
2019-10-02 20:29:11 -0400

    Fixed

--- stronghold/find_motif.py
+++ stronghold/find_motif.py
@@ -12,7 +12,7 @@ def main(argv):
         motif = strand.split('\n')[1]
 
     indicies = []
-    for match in re.finditer(motif, dna_strand, flags=re.VERBOSE):
+    for match in re.finditer('(?=' + motif + ')', dna_strand, flags=re.VERBOSE):
         indicies.append(str(match.start() + 1))
     print(' '.join(indicies))
 
